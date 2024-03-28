a0c2f670f13a9935367ec4d11b33b0f7a0abbc1b
ArmPromnam <armpromnam67@gmail.com>
2024-03-28 15:59:05 +0700

    Update Statistic.py

---
+++ pages/Statistic.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+import requests
+import pandas as pd
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+html_1 = """
+<div style="background-color:#32C021;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ดัชนีสมรรถนะสิ่งแวดล้อมของประเทศไทย</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
+
+
+df=pd.read_csv("./data/TH.csv")
+st.subheader("ดัชนีสมรรถนะสิ่งแวดล้อมของประเทศไทย")
+st.write(df.head(10))
+
