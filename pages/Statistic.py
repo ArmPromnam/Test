ad1e8a9d1da5bcdbb556143b094febc6e4e92be4
ArmPromnam <armpromnam67@gmail.com>
2024-03-28 16:01:15 +0700

    Update Statistic.py

--- pages/Statistic.py
+++ pages/Statistic.py
@@ -23,3 +23,11 @@ df=pd.read_csv("./data/TH.csv")
 st.subheader("ดัชนีสมรรถนะสิ่งแวดล้อมของประเทศไทย")
 st.write(df.head(10))
 
+st.subheader("สถิติ")
+st.write('ผลรวม')
+cl1,cl2,cl3,cl4=st.columns(4)
+cl1.write(dt['sepal.length'].sum())
+cl2.write(dt['sepal.width'].sum())
+cl3.write(dt['petal.length'].sum())
+cl4.write(dt['petal.width'].sum())
+
