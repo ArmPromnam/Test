a83197d6b1f560d332e4474197c1a5d93e25cd07
ArmPromnam <armpromnam67@gmail.com>
2024-03-28 15:51:13 +0700

    Update home.py

--- home.py
+++ home.py
@@ -16,7 +16,7 @@ st_lottie(lottie_hello,key="hello")
 
 html_1 = """
 <div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+<center><h5>ดัชนีสมรรถนะสิ่งแวดล้อมของประเทศไทย</h5></center>
 </div>
 """
 st.markdown(html_1, unsafe_allow_html=True)
